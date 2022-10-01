e2b90e958eac64255dadec3dcf8cb25816599981
Pulido1307 <52903296+Pulido1307@users.noreply.github.com>
2022-10-01 14:47:20 -0500

    [ADD] All the activities

---
+++ app/src/main/java/com/polar/industries/teskotlin/GananciasActivity.kt
@@ -0,0 +1,11 @@
+package com.polar.industries.teskotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GananciasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ganancias)
+    }
+}
---
+++ app/src/main/java/com/polar/industries/teskotlin/MensajeriaActivity.kt
@@ -0,0 +1,11 @@
+package com.polar.industries.teskotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MensajeriaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mensajeria)
+    }
+}
